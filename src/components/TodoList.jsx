a0a34349ba6f0ebd6a40b1b24b8fcdc5b969049a
MarFandino84 <104876435+MarFandino84@users.noreply.github.com>
2024-01-04 19:57:31 -0300

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { TodoHeaderStyle, RenderListStyle} from './TodoListStyles';
+import { useState,useEffect } from 'react';
+
+
+const TodoList = () => {
+ 
+const [input, setInput] = useState("");
+const [task, setTask] = useState(JSON.parse(localStorage.getItem(`taskAdd`)) || [])
+const [check, setCheck] = useState(false)
+
+const handleClick = (e) => {
+   e.preventDefault();
+   setTask([ ...task, { 
+      todos: input,
+      id: Math.random(),
+     }]),
+  setInput("")
+  
+}
+useEffect(() => {
+  localStorage.setItem( `taskAdd`, JSON.stringify(task))   
+},[task])
+
+const handleClickDelete = (pass) => { 
+     let list = task.filter( newlist => {
+       return  newlist.id !== pass
+
+     })
+    setTask(list);
+}
+
+
+  return (
+    <>
+    <TodoHeaderStyle>
+          <label >New Task</label>        
+          <input id="input" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Waiting ...' ></input>
+           <button onClick={handleClick}>Add</button>
+      
+      
+      </TodoHeaderStyle>   
+      { 
+        task?.map( todo => {
+          const {id, todos} = todo;
+
+        return <RenderListStyle key={id}> 
+          <li>{todos}</li> <input id="check" name="name" type='checkbox'/>
+            <button onClick={() => handleClickDelete(id)}>Delete</button>
+            </RenderListStyle>
+        })
+
+      }
+
+    </>
+  )
+}
+
+
+
+export default TodoList
